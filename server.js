1088fce19f0db4544983aa5ed47cfd44617e1638
Ian Wu <iwle@u.nus.edu>
2019-07-20 13:43:54 +0800

    Update to include handler for email

--- server.js
+++ server.js
@@ -20,6 +20,40 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/thanks', function(req, res) {
+  res.sendFile(path.join(__dirname + '/thanks.html'));
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/projects', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/team', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/signup', function(req, res) {
+  res.sendFile(path.join(__dirname + '/signup.html'));
+});
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+app.post('/', function(request, response) {
+  // console.log("I got a POST req");
+  console.log("Got email: " + request.body.email);
+  client.query(`INSERT INTO public.mail(email) VALUES '${request.body.email}'`, (err, res) => {
+    if (err) throw err;
+    client.end();
+  })
+});
+
 http.listen(port, function() {
   console.log(`Listening on ${port}.`);
 })
