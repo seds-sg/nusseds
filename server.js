778520c55ee29fa4c988283e3c3409284aea2a17
Muruges <muruges95@hotmail.com>
2019-10-15 16:21:10 +0800

    updated descriptions and links

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 
@@ -8,8 +7,8 @@ const port = process.env.PORT || 3000;
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
 });
 
 client.connect();
@@ -24,65 +23,67 @@ app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')))
 app.use('/scss', express.static(path.join(__dirname + '/scss')));
 app.use('/vendor', express.static(path.join(__dirname + '/vendor')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/thanks', function (req, res) {
-  res.sendFile(path.join(__dirname + '/thanks.html'));
+app.get('/thanks', function(req, res) {
+	res.sendFile(path.join(__dirname + '/thanks.html'));
 });
 
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/about', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/projects', function (req, res) {
-  res.sendFile(path.join(__dirname + '/projects.html'));
+app.get('/projects', function(req, res) {
+	res.sendFile(path.join(__dirname + '/projects.html'));
 });
 
-app.get('/team', function (req, res) {
-  res.sendFile(path.join(__dirname + '/team.html'));
+app.get('/team', function(req, res) {
+	res.sendFile(path.join(__dirname + '/team.html'));
 });
 
-app.get('/events', function (req, res) {
-  res.sendFile(path.join(__dirname + '/events.html'));
+app.get('/events', function(req, res) {
+	res.sendFile(path.join(__dirname + '/events.html'));
 });
 
-app.get('/presentation', function (req, res) {
-  res.sendFile(path.join(__dirname + '/presentation.pdf'));
-})
+app.get('/presentation', function(req, res) {
+	res.sendFile(path.join(__dirname + '/presentation.pdf'));
+});
 
-app.get('/poster', function (req, res) {
-  res.sendFile(path.join(__dirname + '/poster.pdf'));
-})
+app.get('/poster', function(req, res) {
+	res.sendFile(path.join(__dirname + '/poster.pdf'));
+});
 
-app.get('/rover-challenge', function (req, res) {
-  res.sendFile(path.join(__dirname + '/rover-challenge.html'));
-})
+app.get('/new-opportunity', function(req, res) {
+	res.sendFile(path.join(__dirname + '/new-opportunity.html'));
+});
 
-app.get('/darpa-subt', function (req, res) {
-  res.sendFile(path.join(__dirname + '/darpa-subt.html'));
-})
+app.get('/darpa-subt', function(req, res) {
+	res.sendFile(path.join(__dirname + '/darpa-subt.html'));
+});
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+);
 app.use(express.json());
 
-app.post('/', function (request, response) {
-  console.log("Got email: " + request.body.email);
-  var query = `INSERT INTO public.mail(email, submit_date) VALUES ('${request.body.email}', current_date);`
-  console.log(query);
-  client.query(query, (err, res) => {
-    if (err) {
-      console.log(err);
-    };
-    client.end();
-  });
+app.post('/', function(request, response) {
+	console.log('Got email: ' + request.body.email);
+	var query = `INSERT INTO public.mail(email, submit_date) VALUES ('${request.body.email}', current_date);`;
+	console.log(query);
+	client.query(query, (err, res) => {
+		if (err) {
+			console.log(err);
+		}
+		client.end();
+	});
 
-  response.redirect('/thanks');
+	response.redirect('/thanks');
 });
 
-http.listen(port, function () {
-  console.log(`Listening on ${port}.`);
-})
+http.listen(port, function() {
+	console.log(`Listening on ${port}.`);
+});
